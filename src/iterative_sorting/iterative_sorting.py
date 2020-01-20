1b896d1f7b4aee544545c02a7541afae1dc7a9ac
sriley <sarahriley.dev@gmail.com>
2020-01-20 12:29:34 -0500

    selection sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,17 +1,20 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, len(arr)):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
-
-
+        for j in range(cur_index + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
-
+        if smallest_index != cur_index:
+            smaller = arr[smallest_index]
+            arr[smallest_index] = arr[cur_index]
+            arr[cur_index] = smaller
 
 
 
